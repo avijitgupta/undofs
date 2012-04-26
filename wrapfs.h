067e563946d0fb9535295f98d03588b0c6bb095a
Avijit Gupta <mailforavijit@gmail.com>
2012-04-26 02:54:53 -0400

    wrapfs.h commit

---
+++ wrapfs.h
@@ -0,0 +1,261 @@
+/*
+ * Copyright (c) 1998-2011 Erez Zadok
+ * Copyright (c) 2009	   Shrikar Archak
+ * Copyright (c) 2003-2011 Stony Brook University
+ * Copyright (c) 2003-2011 The Research Foundation of SUNY
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _WRAPFS_H_
+#define _WRAPFS_H_
+
+#include <linux/dcache.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/mm.h>
+#include <linux/mount.h>
+#include <linux/namei.h>
+#include <linux/seq_file.h>
+#include <linux/statfs.h>
+#include <linux/fs_stack.h>
+#include <linux/magic.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/namei.h>
+#include <linux/pagemap.h>
+#include <linux/fsnotify.h>
+#include <linux/personality.h>
+#include <linux/security.h>
+#include <linux/ima.h>
+#include <linux/syscalls.h>
+#include <linux/mount.h>
+#include <linux/audit.h>
+#include <linux/capability.h>
+#include <linux/file.h>
+#include <linux/fcntl.h>
+#include <linux/device_cgroup.h>
+#include <linux/fs_struct.h>
+#include <linux/posix_acl.h>
+#include <asm/uaccess.h>
+/* the file system name */
+#define WRAPFS_NAME "wrapfs"
+
+/* wrapfs root inode number */
+#define WRAPFS_ROOT_INO     1
+
+/* useful for tracking code reachability */
+#define UDBG printk(KERN_DEFAULT "DBG:%s:%s:%d\n", __FILE__, __func__, __LINE__)
+
+/* operations vectors defined in specific files */
+extern const struct file_operations wrapfs_main_fops;
+extern const struct file_operations wrapfs_dir_fops;
+extern const struct inode_operations wrapfs_main_iops;
+extern const struct inode_operations wrapfs_dir_iops;
+extern const struct inode_operations wrapfs_symlink_iops;
+extern const struct super_operations wrapfs_sops;
+extern const struct dentry_operations wrapfs_dops;
+extern const struct address_space_operations wrapfs_aops, wrapfs_dummy_aops;
+extern const struct vm_operations_struct wrapfs_vm_ops;
+//extern int user_path_parent(int dfd, const char __user *path, struct nameidata *nd, char **name);
+//extern struct dentry *lookup_hash(struct nameidata *nd);
+extern int wrapfs_init_inode_cache(void);
+extern void wrapfs_destroy_inode_cache(void);
+extern int wrapfs_init_dentry_cache(void);
+extern void wrapfs_destroy_dentry_cache(void);
+extern int new_dentry_private_data(struct dentry *dentry);
+extern void free_dentry_private_data(struct dentry *dentry);
+extern struct dentry *wrapfs_lookup(struct inode *dir, struct dentry *dentry,
+				    struct nameidata *nd);
+extern struct inode *wrapfs_iget(struct super_block *sb,
+				 struct inode *lower_inode);
+extern int wrapfs_interpose(struct dentry *dentry, struct super_block *sb,
+			    struct path *lower_path);
+extern struct dentry *wrapfs_lookup(struct inode *dir, struct dentry *dentry,
+                             struct nameidata *nd);
+extern int wrapfs_mkdir(struct inode *dir, struct dentry *dentry, int mode);
+extern ssize_t wrapfs_getxattr_lower(struct dentry *lower_dentry, const char *name,
+                        void *value, size_t size);
+extern ssize_t wrapfs_getxattr(struct dentry *dentry, const char *name, void *value,
+                         size_t size);
+extern int wrapfs_setxattr(struct dentry *dentry, const char *name,
+                     const void *value, size_t size, int flags);
+extern int wrapfs_removexattr(struct dentry *dentry, const char *name);
+extern ssize_t wrapfs_listxattr(struct dentry *dentry, char *list, size_t size);
+extern int wrapfs_d_revalidate(struct dentry *dentry, struct nameidata *nd);
+extern int restore(char* file_name, struct super_block* sb);
+extern int trashbin_file_delete(char* file_name, struct super_block *sb);
+
+
+/* file private data */
+struct wrapfs_file_info {
+	struct file *lower_file;
+	const struct vm_operations_struct *lower_vm_ops;
+};
+
+/* wrapfs inode data in memory */
+struct wrapfs_inode_info {
+	struct inode *lower_inode;
+	struct inode vfs_inode;
+};
+
+/* wrapfs dentry data in memory */
+struct wrapfs_dentry_info {
+	spinlock_t lock;	/* protects lower_path */
+	struct path lower_path;
+};
+
+/* wrapfs super-block data in memory */
+struct wrapfs_sb_info {
+	struct super_block *lower_sb;
+	struct dentry* trashbin_dentry;
+};
+
+/*
+ * inode to private data
+ *
+ * Since we use containers and the struct inode is _inside_ the
+ * wrapfs_inode_info structure, WRAPFS_I will always (given a non-NULL
+ * inode pointer), return a valid non-NULL pointer.
+ */
+static inline struct wrapfs_inode_info *WRAPFS_I(const struct inode *inode)
+{
+	return container_of(inode, struct wrapfs_inode_info, vfs_inode);
+}
+
+/* dentry to private data */
+#define WRAPFS_D(dent) ((struct wrapfs_dentry_info *)(dent)->d_fsdata)
+
+/* superblock to private data */
+#define WRAPFS_SB(super) ((struct wrapfs_sb_info *)(super)->s_fs_info)
+
+/* file to private Data */
+#define WRAPFS_F(file) ((struct wrapfs_file_info *)((file)->private_data))
+
+/* file to lower file */
+
+static inline int set_trashbin_dentry(struct super_block* sb, struct dentry* trash_dentry)
+{
+	//Can there be more error handling??
+
+	if(!trash_dentry)
+	return -EINVAL;
+	else 
+	{
+		WRAPFS_SB(sb)->trashbin_dentry = dget(trash_dentry);
+	//	dget(trash_dentry);
+		return 0;
+	}
+}
+
+static inline struct dentry* get_trashbin_dentry(struct super_block * sb)
+{
+	return WRAPFS_SB(sb)->trashbin_dentry;
+}
+
+static inline struct file *wrapfs_lower_file(const struct file *f)
+{
+	return WRAPFS_F(f)->lower_file;
+}
+
+static inline void wrapfs_set_lower_file(struct file *f, struct file *val)
+{
+	WRAPFS_F(f)->lower_file = val;
+}
+
+/* inode to lower inode. */
+static inline struct inode *wrapfs_lower_inode(const struct inode *i)
+{
+	return WRAPFS_I(i)->lower_inode;
+}
+
+static inline void wrapfs_set_lower_inode(struct inode *i, struct inode *val)
+{
+	WRAPFS_I(i)->lower_inode = val;
+}
+
+/* superblock to lower superblock */
+static inline struct super_block *wrapfs_lower_super(
+	const struct super_block *sb)
+{
+	return WRAPFS_SB(sb)->lower_sb;
+}
+
+static inline void wrapfs_set_lower_super(struct super_block *sb,
+					  struct super_block *val)
+{
+	WRAPFS_SB(sb)->lower_sb = val;
+	WRAPFS_SB(sb)->trashbin_dentry = NULL; //Initialise the value of trashbin dentry
+}
+
+/* path based (dentry/mnt) macros */
+static inline void pathcpy(struct path *dst, const struct path *src)
+{
+	dst->dentry = src->dentry;
+	dst->mnt = src->mnt;
+}
+/* Returns struct path.  Caller must path_put it. */
+static inline void wrapfs_get_lower_path(const struct dentry *dent,
+					 struct path *lower_path)
+{
+	spin_lock(&WRAPFS_D(dent)->lock);
+	pathcpy(lower_path, &WRAPFS_D(dent)->lower_path);
+	path_get(lower_path);
+	spin_unlock(&WRAPFS_D(dent)->lock);
+	return;
+}
+static inline void wrapfs_put_lower_path(const struct dentry *dent,
+					 struct path *lower_path)
+{
+	path_put(lower_path);
+	return;
+}
+static inline void wrapfs_set_lower_path(const struct dentry *dent,
+					 struct path *lower_path)
+{
+	spin_lock(&WRAPFS_D(dent)->lock);
+	pathcpy(&WRAPFS_D(dent)->lower_path, lower_path);
+	spin_unlock(&WRAPFS_D(dent)->lock);
+	return;
+}
+static inline void wrapfs_reset_lower_path(const struct dentry *dent)
+{
+	spin_lock(&WRAPFS_D(dent)->lock);
+	WRAPFS_D(dent)->lower_path.dentry = NULL;
+	WRAPFS_D(dent)->lower_path.mnt = NULL;
+	spin_unlock(&WRAPFS_D(dent)->lock);
+	return;
+}
+static inline void wrapfs_put_reset_lower_path(const struct dentry *dent)
+{
+	struct path lower_path;
+	spin_lock(&WRAPFS_D(dent)->lock);
+	pathcpy(&lower_path, &WRAPFS_D(dent)->lower_path);
+	WRAPFS_D(dent)->lower_path.dentry = NULL;
+	WRAPFS_D(dent)->lower_path.mnt = NULL;
+	spin_unlock(&WRAPFS_D(dent)->lock);
+	path_put(&lower_path);
+	return;
+}
+
+/* locking helpers */
+static inline struct dentry *lock_parent(struct dentry *dentry)
+{
+	struct dentry *dir = dget_parent(dentry);
+	mutex_lock_nested(&dir->d_inode->i_mutex, I_MUTEX_PARENT);
+	return dir;
+}
+
+static inline void unlock_dir(struct dentry *dir)
+{
+	mutex_unlock(&dir->d_inode->i_mutex);
+	dput(dir);
+}
+#endif	/* not _WRAPFS_H_ */
